780bc517e86e4feee9b77cec23146c6e4cbd3bcb
nakatani-takashi <nakatani_takashi@arsaga.jp>
2021-08-31 17:44:24 +0900

    モーダルに閉じる機能追加 リザルトテキストの変更機能 ログ上でNFCのidm確認できるようにした。

--- app/src/main/java/com/example/nfc_test/MainActivity.kt
+++ app/src/main/java/com/example/nfc_test/MainActivity.kt
@@ -3,36 +3,38 @@ package com.example.nfc_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nfc_test.ui.theme.NFC_TESTTheme
 import kotlinx.coroutines.launch
+import android.nfc.NfcAdapter
+import android.nfc.NfcAdapter.ReaderCallback
+import android.nfc.Tag
+import android.util.Log
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.tooling.preview.Preview
+import java.lang.StringBuilder
+import java.util.*
+
 
 class MainActivity : ComponentActivity() {
+
+    var nfcAdapter: NfcAdapter? = null
+    var activity = this@MainActivity
+
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var content = "testtest"
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         setContent {
             NFC_TESTTheme {
-                NfcTest(content)
+                NfcTest(nfcAdapter, activity)
             }
         }
     }
@@ -40,24 +42,31 @@ class MainActivity : ComponentActivity() {
 
 @ExperimentalMaterialApi
 @Composable
-fun NfcTest(content: String) {
+fun NfcTest(nfcAdapter: NfcAdapter?, activity: MainActivity?) {
     val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
+
+    var id by rememberSaveable { mutableStateOf("") }
+
     ModalBottomSheetLayout(
         sheetState = state,
         sheetContent = {
             Column(
                 modifier = Modifier.padding(24.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
                 Text(text = "スキャンの準備が出来ました")
-                
+
                 Box(
                     modifier = Modifier.size(56.dp)
                 )
 
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        id = "キャンセル"
+                        nfcAdapter?.disableReaderMode(activity)
+                        scope.launch { state.hide() }
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(
@@ -85,16 +94,20 @@ fun NfcTest(content: String) {
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = content,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .border(width = 4.dp, color = Color.Black)
-                        .padding(32.dp)
-                )
+
+                ResultId(id = id, onIdChange = { id = it })
+
                 Button(
-                    onClick = { scope.launch { state.show() } },
+                    onClick = {
+                        id = "スキャン"
+                        nfcAdapter?.enableReaderMode(
+                            activity,
+                            MyReaderCallback(),
+                            NfcAdapter.FLAG_READER_NFC_F,
+                            null
+                        )
+                        scope.launch { state.show() }
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(
@@ -107,9 +120,39 @@ fun NfcTest(content: String) {
     }
 }
 
+@Composable
+fun ResultId(id: String, onIdChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = id,
+        onValueChange = onIdChange,
+        label = { Text("Read ID ..") }
+    )
+}
+
+private class MyReaderCallback : ReaderCallback {
+    override fun onTagDiscovered(tag: Tag) {
+        Log.d("Hoge", "Tag discoverd.")
+
+        //get idm
+        val idm = tag.id
+        val idmString = bytesToHexString(idm)
+        Log.d("Hoge", idmString)
+    }
+}
+
+fun bytesToHexString(bytes: ByteArray): String {
+    val sb = StringBuilder()
+    val formatter = Formatter(sb)
+    for (b in bytes) {
+        formatter.format("%02x", b)
+    }
+    return sb.toString().uppercase(Locale.getDefault())
+}
+
+
 @ExperimentalMaterialApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    NfcTest(content = "")
+    NfcTest(null, null)
 }
