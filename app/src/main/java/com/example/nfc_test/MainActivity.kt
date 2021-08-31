4041520a8d68aca8d909356287db0942b36c8306
nakatani-takashi <nakatani_takashi@arsaga.jp>
2021-08-31 15:58:55 +0900

    test

--- app/src/main/java/com/example/nfc_test/MainActivity.kt
+++ app/src/main/java/com/example/nfc_test/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var content = "test"
+        var content = "testtest"
 
         setContent {
             NFC_TESTTheme {
